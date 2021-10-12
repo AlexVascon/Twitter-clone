134d3d0d561f688addd68e907f8f911f1be29cd9
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-10-12 21:25:59 +0200

    feat: add password check component for login page

---
+++ client/src/components/loginPassword/LoginPassword.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './loginPassword.css'
+
+export default function LoginPassword() {
+    return (
+        <div>
+            <h1>Password Step</h1>
+        </div>
+    )
+}
