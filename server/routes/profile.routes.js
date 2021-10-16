63c27247544a7f2a7da7e30fc695f9cd96ccec29
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-10-16 18:00:06 +0200

    feat: add upload profile and cover image,get liked tweets, edit user details and clean code

---
+++ server/routes/profile.routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const ProfileController = require('../controllers/profile.controller');
+const fileUploader = require('../config/cloudinary.config');
+
+
+router.post('/setup/upload/profilePicture', fileUploader.single('profilePicture'), ProfileController.profile_image_upload);
+
+router.post('/setup/upload/coverPicture', fileUploader.single('coverPicture'), ProfileController.profile_cover_upload);
+
+router.get('/liked/tweets', ProfileController.get_liked_tweets);
+
+router.post('/edit', ProfileController.profile_edit);
+
+router.get('/details', ProfileController.profile_details);
+
+
+module.exports = router;
