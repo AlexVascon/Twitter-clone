ac7961241a4499732dcd05bccf17b1c379d877c9
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-10-17 00:10:46 +0200

    fix: fix error handling and conditional render structure

--- client/src/pages/feed/Feed.jsx
+++ client/src/pages/feed/Feed.jsx
@@ -16,29 +16,31 @@ import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Dialog from "@mui/material/Dialog";
 import IconButton from "@mui/material/IconButton";
 import Slide from "@mui/material/Slide";
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import ChatBubbleOutlineRoundedIcon from '@mui/icons-material/ChatBubbleOutlineRounded';
-import RepeatRoundedIcon from '@mui/icons-material/RepeatRounded';
-import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded';
-import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
-import CloseIcon from '@mui/icons-material/Close';
-import { useHistory } from 'react-router-dom';
-
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import ChatBubbleOutlineRoundedIcon from "@mui/icons-material/ChatBubbleOutlineRounded";
+import RepeatRoundedIcon from "@mui/icons-material/RepeatRounded";
+import FavoriteBorderRoundedIcon from "@mui/icons-material/FavoriteBorderRounded";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import CloseIcon from "@mui/icons-material/Close";
+import { useHistory } from "react-router-dom";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
 export default function Feed() {
-
   const history = useHistory();
 
   const { logOutUser } = useContext(AuthContext);
 
   const [user, setUser] = useState("");
   const [users, setUsers] = useState([]);
+  const [pendingUsers, setPendingUsers] = useState(true);
+  const [usersError, setUsersError] = useState(undefined);
   const [restOfUsers, setRestOfUsers] = useState([]);
   const [followingTweets, setFollowingTweets] = useState(null);
+  const [pendingFollowingTweets, setPendingFollowingTweets] = useState(true);
+  const [followingTweetsError, setFollowingTweetsError] = useState(undefined);
   const [state, setState] = useState({ left: false });
   const [open, setOpen] = useState(false);
 
@@ -51,8 +53,8 @@ export default function Feed() {
   };
 
   const clickToProfile = () => {
-      history.push('/profile');
-  }
+    history.push("/profile");
+  };
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (
@@ -74,19 +76,23 @@ export default function Feed() {
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <List>
-      <div className='sidebar-header'>
+        <div className="sidebar-header">
           <h4>Account info</h4>
           <CloseIcon />
-      </div>
-      <Divider />
-      <div className='sidebar-user'>
-          <Avatar src={user.profilePicture} sx={{ width: 40, height: 40 }} onClick={() => clickToProfile()} />
+        </div>
+        <Divider />
+        <div className="sidebar-user">
+          <Avatar
+            src={user.profilePicture}
+            sx={{ width: 40, height: 40 }}
+            onClick={() => clickToProfile()}
+          />
           <h4>{user.name}</h4>
           <p>
-                <span>{user.following?.length}</span> Following{" "}
-                <span>{user.followers?.length}</span> Followers{" "}
-              </p>
-      </div>
+            <span>{user.following?.length}</span> Following{" "}
+            <span>{user.followers?.length}</span> Followers{" "}
+          </p>
+        </div>
         {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
           <ListItem button key={text}>
             <ListItemIcon>
@@ -117,7 +123,6 @@ export default function Feed() {
       try {
         const loggedUserDetails = await authAxios.get("profile/details");
         setUser(loggedUserDetails?.data?.user);
-
       } catch (err) {
         console.error(err);
       }
@@ -128,19 +133,19 @@ export default function Feed() {
   useEffect(() => {
     const getUsersNotFollowing = async () => {
       try {
-        const usersData = await authAxios.get('user/all/non/following');
+        const usersData = await authAxios.get("user/all/non/following");
         if (usersData?.data?.users?.length <= 3) {
-            setRestOfUsers(usersData?.data?.usersList);
-            setUsers(usersData?.data?.usersList);
-            
+          setRestOfUsers(usersData?.data?.usersList);
+          setUsers(usersData?.data?.usersList);
         } else {
-            setRestOfUsers(usersData?.data?.usersList);
-            setUsers(usersData?.data?.usersList?.slice(0,3));
-            
+          setRestOfUsers(usersData?.data?.usersList);
+          setUsers(usersData?.data?.usersList?.slice(0, 3));
         }
-        
       } catch (err) {
         console.error(err);
+        setUsersError(err.message);
+      } finally {
+        setPendingUsers(false);
       }
     };
     getUsersNotFollowing();
@@ -150,35 +155,35 @@ export default function Feed() {
     try {
       const data = { userId };
       await authAxios.post("user/follow", data);
-
     } catch (err) {
       console.error(err);
     }
   };
 
   const getFollowingTweets = async () => {
-      try {
-          const res = await authAxios.get('user/following/tweets');
-          setFollowingTweets(res?.data?.followingTweets);
-
-      } catch (err) {
-          console.error(err);
-      }
-  }
+    try {
+      const res = await authAxios.get("user/following/tweets");
+      setFollowingTweets(res?.data?.followingTweets);
+    } catch (err) {
+      console.error(err);
+      setFollowingTweetsError(err.message);
+    } finally {
+      setPendingFollowingTweets(false);
+    }
+  };
 
   useEffect(() => {
     getFollowingTweets();
-  }, [])
+  }, []);
 
   const likedTweet = async (tweetId) => {
-      try {
-          const data = { tweetId }
-          await authAxios.post('user/like/tweet', data);
-    
-      } catch (err) {
-          console.error(err);
-      }
-  }
+    try {
+      const data = { tweetId };
+      await authAxios.post("user/like/tweet", data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <div>
@@ -201,27 +206,25 @@ export default function Feed() {
         <h4 className="feed-title">Feed</h4>
       </div>
       <Divider />
-      {users
-        ? users.map((user) => (
-            <div className="follow-suggestions">
-              <Avatar
-                className="user-avatar"
-                sx={{ width: 50, height: 50 }}
-                src={user.profilePicture}
-              />
-              <div className="user-description">
-                <h4 className="user-name">{user.name}</h4>
-                <p className="user-bio">{user.bio}</p>
-              </div>
-              <button
-                className="follow-btn"
-                onClick={() => followUser(user._id)}
-              >
-                Follow
-              </button>
+      {usersError && <p>{usersError}</p>}
+      {pendingUsers && "loading..."}
+      {users &&
+        users.map((user) => (
+          <div className="follow-suggestions">
+            <Avatar
+              className="user-avatar"
+              sx={{ width: 50, height: 50 }}
+              src={user.profilePicture}
+            />
+            <div className="user-description">
+              <h4 className="user-name">{user.name}</h4>
+              <p className="user-bio">{user.bio}</p>
             </div>
-          ))
-        : "loading..."}
+            <button className="follow-btn" onClick={() => followUser(user._id)}>
+              Follow
+            </button>
+          </div>
+        ))}
       <div>
         <div className="show-more-btn-container">
           <button className="show-more-btn" onClick={() => handleUsersExpand()}>
@@ -244,54 +247,60 @@ export default function Feed() {
           <Divider />
           <div className="">
             <h3 className="users-list-title">Suggested for you</h3>
-            {restOfUsers
-              ? restOfUsers.map((user) => (
-                  <div className="follow-suggestions">
-                    <Avatar
-                      className="user-avatar"
-                      sx={{ width: 50, height: 50 }}
-                      src={user.profilePicture}
-                    />
-                    <div className="user-description">
-                      <h4 className="user-name">{user.name}</h4>
-                      <p className="user-bio">{user.bio}</p>
-                    </div>
-                    <button
-                      className="follow-btn"
-                      onClick={() => followUser(user._id)}
-                    >
-                      Follow
-                    </button>
+            {restOfUsers &&
+              restOfUsers.map((user) => (
+                <div className="follow-suggestions">
+                  <Avatar
+                    className="user-avatar"
+                    sx={{ width: 50, height: 50 }}
+                    src={user.profilePicture}
+                  />
+                  <div className="user-description">
+                    <h4 className="user-name">{user.name}</h4>
+                    <p className="user-bio">{user.bio}</p>
                   </div>
-                ))
-              : "loading..."}
+                  <button
+                    className="follow-btn"
+                    onClick={() => followUser(user._id)}
+                  >
+                    Follow
+                  </button>
+                </div>
+              ))}
           </div>
           <div className="input-options"></div>
         </Dialog>
       </div>
-      <div className='following-tweets-list'>
-          {followingTweets ? followingTweets.map((tweet) => {
-                    return (<div className="tweet-container">
-                    <div className="top-half-tweet">
-                      <Avatar
-                        src={tweet.profilePicture}
-                        sx={{ width: 50, height: 50 }}
-                      />
-                      <div className="top-middle-tweet">
-                        <h4 className='tweet-creator-name'>{tweet.name}</h4>
-                        <p className='tweet-description'>{tweet.description}</p>
-                      </div>
-                      <MoreHorizIcon className='tweet-option-icon' />
-                    </div>
-                    <div className='bottom-half-tweet'>
-                        <ChatBubbleOutlineRoundedIcon />
-                        <RepeatRoundedIcon />
-                        <FavoriteBorderRoundedIcon onClick={() => likedTweet(tweet.tweetId)} />
-                        <ShareOutlinedIcon />
-                    </div>
-                  </div>)
-                }) : 'loading...'}
-          </div>
+      <div className="following-tweets-list">
+        {pendingFollowingTweets && "loading..."}
+        {followingTweetsError && <p> {followingTweetsError} </p>}
+        {followingTweets &&
+          followingTweets.map((tweet) => {
+            return (
+              <div className="tweet-container">
+                <div className="top-half-tweet">
+                  <Avatar
+                    src={tweet.profilePicture}
+                    sx={{ width: 50, height: 50 }}
+                  />
+                  <div className="top-middle-tweet">
+                    <h4 className="tweet-creator-name">{tweet.name}</h4>
+                    <p className="tweet-description">{tweet.description}</p>
+                  </div>
+                  <MoreHorizIcon className="tweet-option-icon" />
+                </div>
+                <div className="bottom-half-tweet">
+                  <ChatBubbleOutlineRoundedIcon />
+                  <RepeatRoundedIcon />
+                  <FavoriteBorderRoundedIcon
+                    onClick={() => likedTweet(tweet.tweetId)}
+                  />
+                  <ShareOutlinedIcon />
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -6,6 +6,8 @@ router.get('/following/tweets', UserController.following_tweets);
 
 router.post('/like/tweet', UserController.like_tweet);
 
+router.get('/all/non/following', UserController.get_follow_options);
+
 router.post('/follow', UserController.follow_user);
 
 router.get('/all', UserController.get_all_users);
